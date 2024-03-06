92c26220234803eca49da0fcfa81cf93696a5287
yuliavolos8 <147557438+yuliavolos8@users.noreply.github.com>
2024-03-06 22:23:16 +0100

    CuentaCorriente
    añado clase cuenta corriente

---
+++ CuentaCorriente.java
@@ -0,0 +1,43 @@
+package cuentaCorriente;
+
+public class CuentaCorriente {
+
+	private int numero;
+	private double saldo;
+
+	private CuentaCorriente() {
+		this.numero = (int) (Math.random() * (1000 - 0 + 1) + 0);
+		this.saldo = 0;
+	}
+
+	private CuentaCorriente(int numero) {
+
+		this.numero = numero;
+		this.saldo = 0;
+	}
+
+	private CuentaCorriente(int numero, double saldo) {
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+	public int getNumero() {
+		return this.numero;
+	}
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	private static void anadirDinero(CuentaCorriente cuenta, double cantidad) {
+		cuenta.numero += cantidad;
+	}
+
+	private static void retirarDinero(CuentaCorriente c, double cantidad) throws Exception {
+		if (cantidad > c.saldo) {
+			Exception e = new Exception("ERROR: Saldo insuficiente");
+			throw e;
+		} else {
+			c.saldo -= cantidad;
+		}
+	}
+
+}
