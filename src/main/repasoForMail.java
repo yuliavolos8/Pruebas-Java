246eb8bddb3746d07a001f02d80d2b509564d8e1
yuliavolos8 <147557438+yuliavolos8@users.noreply.github.com>
2024-04-25 17:38:53 +0200

    repaso de for

---
+++ src/main/repasoForMail.java
@@ -0,0 +1,23 @@
+package main;
+
+import javax.swing.JOptionPane;
+
+public class repasoForMail {
+
+	public static void main(String[] args) {
+		String mail = "";
+		boolean arroba = false;
+		mail = JOptionPane.showInputDialog("introduce su correo electronico");
+		for (int i = 0; i < mail.length(); i++) {
+			if (mail.charAt(i) == '@') {
+				arroba = true;
+			}
+		}
+		if (arroba == true) {
+
+			System.out.println(" correcto");
+		} else {
+			System.out.println(" mail no valido");
+		}
+	}
+}
