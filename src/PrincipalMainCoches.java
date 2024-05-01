8abd7023d68937e861116f308e62f656024e0068
yuliavolos8 <147557438+yuliavolos8@users.noreply.github.com>
2024-05-01 17:07:46 +0200

    practicando polimorfismo, he añadido dos objetos de subclase a ArrayList de coche

--- src/PrincipalMainCoches.java
+++ src/PrincipalMainCoches.java
@@ -9,10 +9,13 @@ public class PrincipalMainCoches {
 		Coche coche3 = new Coche(4, "verde", true, "volvo");
 		Coche coche4 = new Coche(4, "verde", true, "volvo");
 
+		Furgoneta miFurgoneta1 = new Furgoneta(7, 580);
 		coches.add(coche3);
 		coches.add(coche2);
 		coches.add(coche1);
 		coches.add(coche4);
+		coches.add(miFurgoneta1);// POLIMORFISMO
+		coches.add(new Furgoneta(8, 600));// polimorfismo
 
 		for (Coche c : coches) {
 			System.out.println(c.dame_Datos());
@@ -20,5 +23,11 @@ public class PrincipalMainCoches {
 		String idSiguiente = Coche.DameIdSiguiente();
 
 		System.out.println(idSiguiente);
+
+		miFurgoneta1.setColor("azul");
+		miFurgoneta1.setCarrroceria(false);
+		miFurgoneta1.dame_Datos();
+		System.out.println(miFurgoneta1.dame_Datos());
+		System.out.println(coches.size());
 	}
 }
